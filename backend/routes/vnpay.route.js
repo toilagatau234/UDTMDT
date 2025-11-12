ab6ff0093c3489491b4152c90085540dc66e929c
toilagatau234 <toilagatau234@gmail.com>
2025-11-12 15:53:22 +0700

    delete all

--- backend/routes/vnpay.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { themThanhtoan, layThanhtoan } from '../controllers/vnpay.controller.js'
-
-const router = express.Router()
-
-router.post("/create_payment_url", themThanhtoan)
-router.get("/vnpay_return", layThanhtoan)
-
-export default router
