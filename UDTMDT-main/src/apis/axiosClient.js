4bc154eb1b9b9f8682dc3a62af35a6d85daa4989
toilagatau234 <156187898+toilagatau234@users.noreply.github.com>
2025-12-17 19:07:24 +0700

    Update axiosClient.js

--- UDTMDT-main/src/apis/axiosClient.js
+++ UDTMDT-main/src/apis/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
+    //baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
+    const baseURL = import.meta.env.VITE_API_URL;
     headers: {
         'Content-Type': 'application/json',
     },
